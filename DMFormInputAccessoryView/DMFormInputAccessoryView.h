386ecb7bb0bfd52fc8795ee208289f0368838e77
fumoboy007 <darrenmo@Darrens-MacBook-Air.local>
2013-04-22 10:51:36 -0400

    Initial implementation.

--- DMFormInputAccessoryView/DMFormInputAccessoryView.h
+++ DMFormInputAccessoryView/DMFormInputAccessoryView.h
@@ -6,8 +6,24 @@
 //  Copyright (c) 2013 Darren Mo. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface DMFormInputAccessoryView : NSObject
+
+@class DMFormInputAccessoryView;
+
+
+@protocol DMFormInputAccessoryViewDataSource <NSObject>
+
+- (UIResponder *)previousResponderForFormInputAccessoryView:(DMFormInputAccessoryView *)formInputAccessoryView;
+- (UIResponder *)nextResponderForFormInputAccessoryView:(DMFormInputAccessoryView *)formInputAccessoryView;
+
+@end
+
+
+@interface DMFormInputAccessoryView : UIToolbar
+
+@property (weak, nonatomic) UIResponder *attachedResponder;
+
+@property (weak, nonatomic) id <DMFormInputAccessoryViewDataSource> dataSource;
 
 @end
--- DMFormInputAccessoryView/DMFormInputAccessoryView.m
+++ DMFormInputAccessoryView/DMFormInputAccessoryView.m
@@ -8,6 +8,98 @@
 
 #import "DMFormInputAccessoryView.h"
 
-@implementation DMFormInputAccessoryView
+
+typedef NS_ENUM(NSUInteger, PrevNextSegment) {
+	PrevSegment = 0,
+	NextSegment
+};
+
+
+@implementation DMFormInputAccessoryView {
+	UISegmentedControl *_prevNextSegmentedControl;
+}
+
+- (id)initWithFrame:(CGRect)frame {
+	self = [super initWithFrame:frame];
+	
+	if (self) {
+		self.barStyle = UIBarStyleBlack;
+		self.translucent = YES;
+		
+		
+		_prevNextSegmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"Previous", @"Next"]];
+		
+		_prevNextSegmentedControl.segmentedControlStyle = UISegmentedControlStyleBar;
+		_prevNextSegmentedControl.momentary = YES;
+		
+		[_prevNextSegmentedControl addTarget:self action:@selector(prevNextSegmentedControlValueChanged) forControlEvents:UIControlEventValueChanged];
+		
+		UIBarButtonItem *prevNextBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:_prevNextSegmentedControl];
+		
+		
+		UIBarButtonItem *flexibleSpaceBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+		
+		
+		UIBarButtonItem *doneBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(doneBarButtonPressed)];
+		
+		
+		self.items = @[prevNextBarButtonItem, flexibleSpaceBarButtonItem, doneBarButtonItem];
+		
+		
+		[self sizeToFit];
+	}
+	
+	return self;
+}
+
+- (void)willMoveToWindow:(UIWindow *)newWindow {
+	[super willMoveToWindow:newWindow];
+	
+	if (newWindow)
+		[self configurePrevNextSegmentedControl];
+}
+
+#pragma mark - Configuring the segmented control
+
+- (void)configurePrevNextSegmentedControl {
+	BOOL prevEnabled = [_dataSource previousResponderForFormInputAccessoryView:self] != nil;
+	BOOL nextEnabled = [_dataSource nextResponderForFormInputAccessoryView:self] != nil;
+	
+	[_prevNextSegmentedControl setEnabled:prevEnabled forSegmentAtIndex:PrevSegment];
+	[_prevNextSegmentedControl setEnabled:nextEnabled forSegmentAtIndex:NextSegment];
+}
+
+#pragma mark - Handling segmented control value changes
+
+- (void)prevNextSegmentedControlValueChanged {
+	switch (_prevNextSegmentedControl.selectedSegmentIndex) {
+		case PrevSegment:
+		{
+			UIResponder *previousResponder = [_dataSource previousResponderForFormInputAccessoryView:self];
+			
+			[previousResponder becomeFirstResponder];
+		}
+			break;
+			
+		case NextSegment:
+		{
+			UIResponder *nextResponder = [_dataSource nextResponderForFormInputAccessoryView:self];
+			
+			[nextResponder becomeFirstResponder];
+		}
+			break;
+			
+		default:
+			break;
+	}
+	
+	[self configurePrevNextSegmentedControl];
+}
+
+#pragma mark - Done pressed
+
+- (void)doneBarButtonPressed {
+	[_attachedResponder resignFirstResponder];
+}
 
 @end
